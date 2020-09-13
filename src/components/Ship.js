690595105f259931861801e906dcd004f02ccf79
Kai Takeshima <takeshimakai@gmail.com>
2020-09-13 10:36:20 -0400

    Basic Ship factory logic and tests

--- src/components/Ship.js
+++ src/components/Ship.js
@@ -1,5 +1,21 @@
-const Ship = () => {
+const Ship = (length) => {
+    const health = [];
 
+    for (let i = 0; i < length; i++) {
+        health.push('o');
+    }
+
+    const hit = (index) => {
+        health.splice(index, 1, 'x');
+        return health;
+    };
+
+    const isSunk = () => health.every((item) => item === 'x');
+
+    return {
+        hit,
+        isSunk,
+    };
 };
 
 export default Ship;
--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,7 +1,28 @@
 import Ship from '../components/Ship';
 
-test('hit() function takes a number and marks position as "hit"', () => {
-    const newShip = Ship();
-    const number = 1;
-    expect(newShip.hit(1)).toBe('hit');
+test('hit() function takes a number and marks position as "x"', () => {
+    const newShip = Ship(4);
+    const num = 1;
+
+    const updatedHealth = newShip.hit(num);
+
+    expect(updatedHealth).toEqual(['o', 'x', 'o', 'o']);
+});
+
+test('isSunk() returns true when all positions have been hit', () => {
+    const newShip = Ship(1);
+
+    newShip.hit(0);
+
+    const isSunk = newShip.isSunk();
+
+    expect(isSunk).toEqual(true);
+});
+
+test('isSunk() returns false when not all positions have been hit', () => {
+    const newShip = Ship(1);
+
+    const isSunk = newShip.isSunk();
+
+    expect(isSunk).toEqual(false);
 });
