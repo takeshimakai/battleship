d2afc2a096c0471b0517b888ba3a5fa83d0461c2
Kai Takeshima <takeshimakai@gmail.com>
2020-10-12 17:39:10 -0400

    add autoAttack()

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,10 +5,17 @@ const Player = (type = 'human') => {
         const y = Math.floor(Math.random() * Math.floor(10));
         const x = Math.floor(Math.random() * Math.floor(10));
 
-        board.receiveAttack(y, x);
+        const cell = board.getBoard()[y][x];
+
+        if (cell === null || cell.hit === false) {
+            board.receiveAttack(y, x);
+        } else {
+            autoAttack(board);
+        }
     };
 
     return {
+        type,
         attack,
         autoAttack,
     };
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,11 +1,12 @@
 import Gameboard from '../components/Gameboard';
 import Player from '../components/Player';
 
+const player = Player();
+const computer = Player('computer');
+const playerBoard = Gameboard();
+const computerBoard = Gameboard();
+
 test('attack() takes a coordinate and attacks enemy board', () => {
-    const computerBoard = Gameboard();
-
-    const player = Player();
-
     player.attack(0, 1, computerBoard);
 
     expect(computerBoard.getBoard()).toEqual(
@@ -24,12 +25,16 @@ test('attack() takes a coordinate and attacks enemy board', () => {
     )
 });
 
-test.only('autoAttack() attacks the board at random coordinates', () => {
-    const playerBoard = Gameboard();
-
-    const computer = Player('computer');
-
-    computer.autoAttack(playerBoard);
-
-    expect(playerBoard.getBoard().map(arr => arr.some(item => item.hit === true))).toBeTruthy();
+describe('autoAttack() functionality', () => {
+    test('autoAttack() attacks at random coordinate on enemy board', () => {
+        computer.autoAttack(playerBoard);
+    
+        expect(playerBoard.getBoard()).toEqual(
+            expect.arrayContaining([
+                expect.arrayContaining([
+                    expect.objectContaining({ hit: true })
+                ])
+            ])
+        );
+    });
 });
