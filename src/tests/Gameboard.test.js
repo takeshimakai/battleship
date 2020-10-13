4c0ca7aa3fa073aeda6ae2712957f27015bf4b9a
Kai Takeshima <takeshimakai@gmail.com>
2020-10-13 18:17:45 -0400

    refactored Gameboard.test.js

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,11 +1,20 @@
 import Gameboard from '../components/Gameboard';
 import Ship from '../components/Ship';
 
-test('Board is a 10x10 grid', () => {
-    const gameboard = Gameboard();
+const gameboard = Gameboard();
+
+afterEach(() => {
     const board = gameboard.getBoard();
 
-    expect(board).toEqual(
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            board[i][j] = null;
+        }
+    }
+});
+
+test('Board is a 10x10 grid', () => {
+    expect(gameboard.getBoard()).toEqual(
         [
             [null, null, null, null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null, null, null, null],
@@ -21,102 +30,94 @@ test('Board is a 10x10 grid', () => {
     );
 });
 
-test('Place ship horizontally', () => {
-    const ship = Ship('ship', 4);
-
-    const gameboard = Gameboard();
-    gameboard.placeShip(ship, 'horizontal', 2, 3);
-
-    const updatedBoard = gameboard.getBoard();
-
-    expect(updatedBoard).toEqual(
-        [
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, { ship, index: 0, hit: false }, { ship, index: 1, hit: false }, { ship, index: 2, hit: false }, { ship, index: 3, hit: false }, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-        ],
-    );
+describe('placeShip() functionality', () => {
+    test('Place ship horizontally', () => {
+        const ship = Ship('ship', 4);
+    
+        gameboard.placeShip(ship, 'horizontal', 2, 3);
+    
+        expect(gameboard.getBoard()).toEqual(
+            [
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, { ship, index: 0, hit: false }, { ship, index: 1, hit: false }, { ship, index: 2, hit: false }, { ship, index: 3, hit: false }, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+            ],
+        );
+    });
+    
+    test('Place ship vertically', () => {
+        const ship = Ship('ship', 4);
+    
+        gameboard.placeShip(ship, 'vertical', 2, 3);
+    
+        expect(gameboard.getBoard()).toEqual(
+            [
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, { ship, index: 0, hit: false }, null, null, null, null, null, null],
+                [null, null, null, { ship, index: 1, hit: false }, null, null, null, null, null, null],
+                [null, null, null, { ship, index: 2, hit: false }, null, null, null, null, null, null],
+                [null, null, null, { ship, index: 3, hit: false }, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+            ],
+        );
+    });
 });
 
-test('Place ship vertically', () => {
-    const ship = Ship('ship', 4);
-
-    const gameboard = Gameboard();
-    gameboard.placeShip(ship, 'vertical', 2, 3);
-
-    const updatedBoard = gameboard.getBoard();
-
-    expect(updatedBoard).toEqual(
-        [
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, { ship, index: 0, hit: false }, null, null, null, null, null, null],
-            [null, null, null, { ship, index: 1, hit: false }, null, null, null, null, null, null],
-            [null, null, null, { ship, index: 2, hit: false }, null, null, null, null, null, null],
-            [null, null, null, { ship, index: 3, hit: false }, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-        ],
-    );
-});
-
-test('receiveAttack function enters object "hit: true" if coordinate is null', () => {
-    const gameboard = Gameboard();
-    gameboard.receiveAttack(0, 1);
-
-    const updatedBoard = gameboard.getBoard();
-
-    expect(updatedBoard).toEqual(
-        [
-            [null, { hit: true }, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null, null, null],
-        ],
-    );
-});
-
-test('receiveAttack function calls hit function if coordinate has a ship', () => {
-    const ship = Ship('ship', 4);
-
-    const gameboard = Gameboard();
-    gameboard.placeShip(ship, 'horizontal', 0, 1);
-    gameboard.receiveAttack(0, 2);
-
-    const shipHealth = ship.getHealth();
-
-    expect(shipHealth).toEqual(['o', 'x', 'o', 'o']);
+describe('receiveAttack() functionality', () => {
+    test('receiveAttack function enters object "hit: true" if coordinate is null', () => {
+        gameboard.receiveAttack(0, 1);
+    
+        expect(gameboard.getBoard()).toEqual(
+            [
+                [null, { hit: true }, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+            ],
+        );
+    });
+    
+    test('receiveAttack function calls hit function if coordinate has a ship', () => {
+        const ship = Ship('ship', 4);
+    
+        gameboard.placeShip(ship, 'horizontal', 0, 1);
+        gameboard.receiveAttack(0, 2);
+    
+        expect(ship.getHealth()).toEqual(['o', 'x', 'o', 'o']);
+    });
 });
 
 test('allShipsSunk() returns false when ships not sunk', () => {
+    const gameboard = Gameboard();
     const ship = Ship('ship', 2);
 
-    const gameboard = Gameboard();
     gameboard.placeShip(ship, 'horizontal', 4, 2);
     gameboard.receiveAttack(4, 2);
 
     expect(gameboard.allShipsSunk()).toBe(false);
 });
 
-test('allShipsSunk() returns true when ships sunk', () => {
+test('allShipsSunk() returns true when all ships sunk', () => {
+    const gameboard = Gameboard();
     const ship = Ship('ship', 2);
 
-    const gameboard = Gameboard();
     gameboard.placeShip(ship, 'horizontal', 4, 2);
     gameboard.receiveAttack(4, 2);
     gameboard.receiveAttack(4, 3);
