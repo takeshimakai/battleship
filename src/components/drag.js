b0260868130029266ecde83b12efb1f893738f1a
Kai Takeshima <takeshimakai@gmail.com>
2020-11-14 18:25:19 -0500

    deleted unnecessary drop function

--- src/components/drag.js
+++ src/components/drag.js
@@ -1,7 +1,3 @@
-import dom from './dom';
-
-const domFunc = dom();
-
 const dragStart = (e) => {
     e.dataTransfer.setData('text', e.target.id);
 };
@@ -11,19 +7,4 @@ const dragOver = (e) => {
     e.dataTransfer.dropEffect = 'copy';
 };
 
-const getDropCoords = (e) => {
-    e.preventDefault();
-    //const data = e.dataTransfer.getData('text');
-
-    const { y, x } = domFunc.getSelectors(e);
-
-    //const shipSquares = document.querySelector(`#${data}`).children;
-
-    //for (let i = 0; i < shipSquares.length; i++) {
-    //    const dropTarget = document.querySelector(`[data-y='${y}'][data-x='${Number(x) + i}']`);
-    //    dropTarget.appendChild(shipSquares[i].cloneNode());
-    //}
-    return { y, x };
-};
-
-export { dragStart, dragOver, getDropCoords };
+export { dragStart, dragOver };
